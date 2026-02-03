e6d79b73f4f7142c39b731ec62a9e5bb9f31d4be
Tanjona <114407464+Tanjona12@users.noreply.github.com>
2026-02-03 20:14:16 +0300

    Crud sur boutique et user coté Admin

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -14,7 +14,12 @@ export const createBoutiqueUser = async (req, res) => {
 
     const { name, email, password, cin } = req.body;
 
-    const existingUser = await User.findOne({ email });
+    const existingUser = await User.findOne({
+      $or: [
+        { email: email },
+        { cin: cin }
+      ]
+    });
     if (existingUser) {
       return res.status(400).json({ message: "Utilisateur déjà existant" });
     }
@@ -64,6 +69,72 @@ export const getBoutiqueUsers = async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
+};
+
+export const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const getUser = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const user = await User.findById(id)
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const changeStatusUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const user = await User.findById(id);
+
+    if (!user) {
+      return res.status(404).json({
+        message: "Utilisateur introuvable",
+      });
+    }
+
+    // Toggle
+    user.isActive = !user.isActive;
+
+    await user.save();
+
+    res.status(200).json({
+      message: `User ${
+        user.isActive ? "activée" : "désactivée"
+      } avec succès`,
+      isActive: user.isActive,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  const id = req.params.id;
+
+  let users;
+  try{
+    users = await User.findByIdAndDelete(id)
+  } catch (err) {
+    console.log(err);
+  }
+  if (!users) {
+    return res.status(500).json({message: "Utilisateur non trouvé"});
+  }
+  return res.status(200).json({message:"Suppréssion succées"});
 };
 
 
@@ -116,4 +187,72 @@ export const createBoutique = async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
+};
+
+
+export const getBoutiques = async (req, res) => {
+  try {
+    const boutiques = await Boutique.find();
+      // .select("name email cin");
+
+    res.status(200).json(boutiques);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const getBoutique = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const boutique = await Boutique.findById(id)
+    res.status(200).json(boutique);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const changeStatusBoutique = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const boutique = await Boutique.findById(id);
+
+    if (!boutique) {
+      return res.status(404).json({
+        message: "Boutique introuvable",
+      });
+    }
+
+    // Toggle
+    boutique.isActive = !boutique.isActive;
+
+    await boutique.save();
+
+    res.status(200).json({
+      message: `Boutique ${
+        boutique.isActive ? "activée" : "désactivée"
+      } avec succès`,
+      isActive: boutique.isActive,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+export const deleteBoutique = async (req, res, next) => {
+  const id = req.params.id;
+
+  let boutiques;
+  try{
+    boutiques = await Boutique.findByIdAndDelete(id)
+  } catch (err) {
+    console.log(err);
+  }
+  if (!boutiques) {
+    return res.status(500).json({message: "Impossible de supprimé, boutique non trouvé"});
+  }
+  return res.status(200).json({message:"Suppréssion succées"});
 };
--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -3,15 +3,25 @@ import { protect } from "../middlewares/auth.js";
 import { authorize } from "../middlewares/role.js";
 import upload from "../middlewares/upload.js";
 
-import { createBoutiqueUser, getBoutiqueUsers, createBoutique } from "../controllers/admin.js";
+import { createBoutiqueUser, getBoutiqueUsers, getUsers, getUser, changeStatusUser, deleteUser,
+    createBoutique, getBoutiques, getBoutique, changeStatusBoutique, deleteBoutique } from "../controllers/admin.js";
 
 const router = express.Router();
 
 // utilisateurs boutique
 router.post("/createUser", protect, authorize("admin"), upload.single("image"), createBoutiqueUser);
-router.get("/getUser", protect, authorize("admin"), getBoutiqueUsers);
+router.get("/getBoutiqueUsers", getBoutiqueUsers);
+router.get("/getUsers", getUsers);
+router.get("/getUser/:id", getUser);
+router.put("/changeStatusUser/:id", protect, authorize("admin"), changeStatusUser);
+router.delete("/deleteUser/:id", protect, authorize("admin"), deleteUser);
+
 
 // boutiques
 router.post("/createBoutique", protect, authorize("admin"), upload.single("logo"), createBoutique);
+router.get("/getBoutiques", getBoutiques);
+router.get("/getBoutique/:id", getBoutique);
+router.put("/changeStatusBoutique/:id", protect, authorize("admin"), changeStatusBoutique);
+router.delete("/deleteBoutique/:id", protect, authorize("admin"), deleteBoutique);
 
 export default router;
